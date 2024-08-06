0068f1d7f7e6384c3e337528608fcff784887927
不讲李 <158622671+domainbf@users.noreply.github.com>
2024-08-07 02:14:08 +0800

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,4 +1,4 @@
-export const VERSION = "不讲李"; // 修改为 "不讲李"
+export const VERSION = "0.2.1"; // 
 export const HISTORY_LIMIT: number = intEnv("NEXT_PUBLIC_HISTORY_LIMIT", 6);
 // The maximum number of history items to keep in the local storage
 
