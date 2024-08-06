766ee34241119af80833e00926dc7eab46fecb71
不讲李 <158622671+domainbf@users.noreply.github.com>
2024-08-06 16:54:24 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -55,11 +55,10 @@ export default function App({ Component, pageProps }: AppProps) {
       className={`w-5 h-5 fill-primary`}
     >
       <title>Domain</title>
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm1-13h-2v6h2V7zm0 8h-2v2h2v-2z" />
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h-2v6h2V7zm0 8h-2v2h2v-2z" />
     </svg>
   </Button>
 </Link>
-
 
           </div>
           <Component {...pageProps} />
