da00edb75fd3ee487326c5d84b0cc7c04b0bfb82
不讲李 <158622671+domainbf@users.noreply.github.com>
2024-08-07 09:03:30 +0800

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,24 +1,6 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
-import { Card as OriginalCard, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./Card";
-
-interface CardWithQRCodeProps extends React.HTMLAttributes<HTMLDivElement> {
-  qrCodeUrl: string;
-}
-
-const CardWithQRCode = React.forwardRef<HTMLDivElement, CardWithQRCodeProps>(
-  ({ className, qrCodeUrl, ...props }, ref) => {
-    const qrCodeSrc = `https://chart.googleapis.com/chart?cht=qr&chs=150x150&chl=${encodeURIComponent(qrCodeUrl)}`;
-
-    return (
-      <OriginalCard ref={ref} className={cn("relative", className)} {...props}>
-        <img src={qrCodeSrc} alt="QR Code" className="absolute bottom-4 right-4 w-16 h-16" />
-        {props.children}
-      </OriginalCard>
-    );
-  }
-);
-CardWithQRCode.displayName = "CardWithQRCode";
+import { CardWithQRCode } from "../CardWithQRCode";
 
 const Card = React.forwardRef<
   HTMLDivElement,
@@ -95,4 +77,3 @@ export {
   CardDescription,
   CardContent,
 };
-
