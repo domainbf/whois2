1cb70ad5bcfcbb82483f67695822ed18ca35dc13
不讲李 <158622671+domainbf@users.noreply.github.com>
2024-08-08 16:57:19 +0800

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
+import QRCode from "qrcode.react"; // 确保导入 qrcode.react
 
+// Card Components
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
@@ -76,6 +77,27 @@ const CardFooter = React.forwardRef<
 ));
 CardFooter.displayName = "CardFooter";
 
+// QRCodeCard Component
+const QRCodeCard = ({ className }) => {
+  const currentUrl = typeof window !== 'undefined' ? window.location.href : ''; // 获取当前页面的 URL
+
+  return (
+    <Card className={className}>
+      <CardHeader>
+        <CardTitle>二维码</CardTitle>
+        <CardDescription>扫描下面的二维码以访问此页面</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <QRCode value={currentUrl} /> {/* 使用当前 URL 生成二维码 */}
+      </CardContent>
+      <CardFooter>
+        <p>这是二维码的底部内容</p>
+      </CardFooter>
+    </Card>
+  );
+};
+
+// 导出组件
 export {
   Card,
   CardHeader,
@@ -83,4 +105,5 @@ export {
   CardTitle,
   CardDescription,
   CardContent,
+  QRCodeCard, // 导出 QRCodeCard
 };
